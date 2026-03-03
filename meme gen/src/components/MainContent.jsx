e73949d069ba727e6b1bc3d41d87a276f6cd3527
ray-17 <simonsharon144@gmail.com>
2026-03-03 06:43:54 +0100

    first commit for meme-gen

---
+++ meme gen/src/components/MainContent.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+
+
+
+export default function MainContent(){
+
+    const [memeInfo, setMemeInfo] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    function handleChange(event){
+        const {value, name} = event.currentTarget
+        console.log(value)
+        setMemeInfo(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    return(
+        <main className="main">
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                        value={memeInfo.topText}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value={memeInfo.bottomText}
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src= {memeInfo.imageUrl}/>
+                <span className="top">{memeInfo.topText}</span>
+                <span className="bottom">{memeInfo.bottomText}</span>
+            </div>
+        </main>
+    )
+}
